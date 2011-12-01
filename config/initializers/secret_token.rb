b8562113eceb4176c480387d49d6ca1b0bfc687d
Xiaofeng Wang <xfwang@thoughtworks.com>
2011-12-01 17:28:14 +0800

    set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniAwayDay::Application.config.secret_token = '4cef1316aaac79832bb5687edc21864a45d7ddeaf60c4b9179d909bfe48f05044868c615c6698b896dc84d915c1c4db033ccd11d4c9511ff22d3f7fc6c3e3ebe'
